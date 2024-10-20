18b2fef6cf47bba7c14e424ea505553ecd8ff02c
Fidarit Mullayanov <fidarit.mullayanov@yandex.ru>
2024-10-21 02:31:48 +0500

    feat: Подготовка проекта

---
+++ MeetingScheduler/MeetingManager.cs
@@ -0,0 +1,43 @@
+﻿namespace MeetingScheduler;
+
+
+internal record Meeting(string Title, DateTime StartTime, DateTime PlannedEndTime, TimeSpan? ReminderTime);
+
+
+internal class MeetingManager
+{
+    public void AddMeeting(Meeting meeting)
+    {
+        if (MeetingHasConflicts(meeting))
+            throw new InvalidOperationException("Встреча конфликтует с существующей встречей.");
+
+        throw new NotImplementedException();
+    }
+
+    public void RemoveMeeting(Meeting meeting)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateMeeting(Meeting oldMeeting, Meeting newMeeting)
+    {
+        RemoveMeeting(oldMeeting);
+        AddMeeting(newMeeting);
+    }
+
+    public List<Meeting> GetMeetings(DateTime date)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ExportMeetingsToFile(DateTime date, string filePath)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    private bool MeetingHasConflicts(Meeting newMeeting)
+    {
+        throw new NotImplementedException();
+    }
+}
--- MeetingScheduler/Program.cs
+++ MeetingScheduler/Program.cs
@@ -1,10 +1,78 @@
-﻿namespace MeetingScheduler
+﻿namespace MeetingScheduler;
+
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var manager = new MeetingManager();
+        bool running = true;
+
+        while (running)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("""
+                Выберите действие:
+                1. Добавить встречу
+                2. Удалить встречу
+                3. Обновить встречу
+                4. Посмотреть встречи
+                5. Экспортировать встречи в файл
+                6. Выйти
+                """);
+
+            var choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    AddMeeting(manager);
+                    break;
+                case "2":
+                    RemoveMeeting(manager);
+                    break;
+                case "3":
+                    UpdateMeeting(manager);
+                    break;
+                case "4":
+                    ViewMeetings(manager);
+                    break;
+                case "5":
+                    ExportMeetings(manager);
+                    break;
+                case "6":
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Неверный выбор. Пожалуйста, попробуйте снова.");
+                    break;
+            }
+
+            Console.WriteLine();
         }
+    }
+
+    static void AddMeeting(MeetingManager manager)
+    {
+        throw new NotImplementedException();
+    }
+
+    static void RemoveMeeting(MeetingManager manager)
+    {
+        throw new NotImplementedException();
+    }
+
+    static void UpdateMeeting(MeetingManager manager)
+    {
+        throw new NotImplementedException();
+    }
+
+    static void ViewMeetings(MeetingManager manager)
+    {
+        throw new NotImplementedException();
+    }
+
+    static void ExportMeetings(MeetingManager manager)
+    {
+        throw new NotImplementedException();
     }
 }
